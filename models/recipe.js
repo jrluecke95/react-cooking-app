45d75d17fbf874587ed2c01397476f50576c3161
Jake Luecke <jrluecke95@gmail.com>
2021-04-17 16:57:25 -0400

    typo =

--- models/recipe.js
+++ models/recipe.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
   };
   Recipe.init({
     title: DataTypes.STRING,
-    recipe: DataTypes.LENGTH
+    recipe: DataTypes.TEXT
   }, {
     sequelize,
     modelName: 'Recipe',
